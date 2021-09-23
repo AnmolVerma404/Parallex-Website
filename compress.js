732151def72815c0e8979a82da5af6e976f9252c
Anmol Verma <anmolverma102002@gmail.com>
2021-09-23 14:53:01 +0530

    Compressed Images using npm compress-images, gifsicle & pngquant-bin

---
+++ compress.js
@@ -0,0 +1,22 @@
+const compress_images = require("compress-images");
+
+function MyFun() {
+  compress_images(
+    "./Images/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}",
+    "CompressedImg/",
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    {
+      gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] },
+    },
+    function (err, completed) {
+      if (completed === true) {
+        // Doing something.
+      }
+    }
+  );
+}
+MyFun();
